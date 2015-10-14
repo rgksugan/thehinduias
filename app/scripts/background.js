4efb35c6421e2283d9afda60545407d210155064
Sugan Krishnan <rgk.sugan@gmail.com>
2015-10-14 15:40:09 +0530

    context menu to add and remove keywords

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -4,23 +4,75 @@
 
 (function() {
 
-  chrome.contextMenus.create({
-    title: 'Add to IAS Hindu keywords',
-    contexts: ['selection'],
-    onclick: function() {
-      console.log('i am clicked');
-      chrome.storage.sync.get('keywords', function() {
-        console.log('i am clicked', arguments);
+  // when installed loads keywords from the fixtures
+  chrome.runtime.onInstalled.addListener(function() {
+    $.get(chrome.extension.getURL('data/keywords.json'), function(response) {
+      var keywords = JSON.parse(response).keywords;
+      chrome.storage.sync.set({
+        'keywords': keywords
       });
+    });
+  });
+
+  // context menu click handler
+  var contextClickHandler = function(selection) {
+    chrome.storage.sync.get('keywords', function(response) {
+      var index = response.keywords.indexOf(selection.selectionText.toLowerCase());
+      if (index < 0) {
+        response.keywords.push(selection.selectionText.toLowerCase());
+      } else {
+        response.keywords.splice(index, 1);
+      }
+      chrome.storage.sync.set({
+        'keywords': response.keywords
+      });
+    });
+  };
+
+  var contextMenu = chrome.contextMenus.create({
+    title: 'Add To TheHinduIAS',
+    contexts: ['selection'],
+    onclick: contextClickHandler
+  });
+
+  chrome.runtime.onMessage.addListener(function(message) {
+    if (message.request === 'updateContextMenu') {
+      var selection = message.selection;
+      if (selection === '') {
+        if (contextMenu !== null) {
+          chrome.contextMenus.remove(contextMenu);
+          contextMenu = null;
+        }
+      } else {
+        var title = '';
+        chrome.storage.sync.get('keywords', function(response) {
+          if (response.keywords.indexOf(selection.toLowerCase()) < 0) {
+            title = 'Add To TheHinduIAS';
+          } else {
+            title = 'Remove From TheHinduIAS';
+          }
+          var options = {
+            title: title,
+            contexts: ['selection'],
+            onclick: contextClickHandler
+          };
+          if (contextMenu !== null) {
+            chrome.contextMenus.update(contextMenu, options);
+          } else {
+            contextMenu = chrome.contextMenus.create(options);
+          }
+        });
+      }
     }
   });
 
+  // add tick or cross based on the keywords
   function checkForValidUrl(tabId, changeInfo, tab) {
     if (tab.url.indexOf('http://www.thehindu.com') === 0) {
 
-      //TODO: remove this, this method should be called after the page is loaded
-      $.get(chrome.extension.getURL('data/keywords.json'), function(response) {
-        var keywords = JSON.parse(response).keywords;
+      chrome.storage.sync.get('keywords', function(response) {
+        var keywords = response.keywords;
+        //TODO: remove this, this method should be called after the page is loaded
         $.get(tab.url, function(response) {
           var article = '';
           $(response).find('.body').each(function(index, text) {
@@ -28,7 +80,7 @@
           });
           var isToBeRead = false;
           keywords.forEach(function(keyword) {
-            if (article.toLowerCase().indexOf(keyword) >= 0) {
+            if (article.toLowerCase().indexOf(keyword.toLowerCase()) >= 0) {
               isToBeRead = true;
             }
           });
--- app/scripts/content-script.js
+++ app/scripts/content-script.js
@@ -4,20 +4,42 @@
 
 (function() {
 
-  $.get(chrome.extension.getURL('data/keywords.json'), function(response) {
-    var keywords = JSON.parse(response).keywords;
-    Array.prototype.slice.call($('.tpaper')).forEach(function(link) {
+  chrome.storage.sync.get('keywords', function(response) {
+    var keywords = response.keywords;
+
+    // adds ticks if the article contains the keywords in http://www.thehindu.com/todays-paper/ page
+    $('.tpaper').each(function(index, link) {
       $.get($(link).find('a').attr('href'), function(response) {
         var article = '';
         $(response).find('.body').each(function(index, text) {
           article += $(text).text();
         });
         keywords.forEach(function(keyword) {
-          if (article.toLowerCase().indexOf(keyword) >= 0) {
-            $(link).addClass('tick');
+          if (article.toLowerCase().indexOf(keyword.toLowerCase()) >= 0) {
+            $(link).addClass('thi-tick');
           }
         });
       });
+    });
+
+    // highlights keywords in the articles
+    $('.body').each(function() {
+      var that = this;
+      keywords.forEach(function(keyword) {
+        if ($(that).text().toLowerCase().indexOf(keyword.toLowerCase()) >= 0) {
+          var replacedWord = $(that).html().match(new RegExp(keyword, 'ig'));
+          $(that).html($(that).html().replace(replacedWord, '<span class="thi-highlight">' + replacedWord + '</span>'));
+        }
+      });
+    });
+  });
+
+  // updates context menu on selection
+  $(document).on('selectionchange', function() {
+    var selection = window.getSelection().toString().trim();
+    chrome.runtime.sendMessage({
+      request: 'updateContextMenu',
+      selection: selection
     });
   });
 
